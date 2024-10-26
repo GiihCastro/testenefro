7f6429d53bf45ee47fd746b3df4cc9f562585e23
Giovanna <160510344+GiihCastro@users.noreply.github.com>
2024-10-26 18:15:43 -0300

    Delete teste.js

--- teste.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('editar-btn').addEventListener('click', function() {
-    const nomeTeste = document.getElementById('nome-teste');
-    const isEditable = nomeTeste.contentEditable === "true";
-
-    nomeTeste.contentEditable = !isEditable;
-    nomeTeste.focus();
-    this.textContent = isEditable ? '🖉' : '✔️'; // Troca ícone entre editar e confirmar
-});
